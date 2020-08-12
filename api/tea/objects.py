d1c15797b4bfe0f4315507475341e1f91b57a123
donuthack <blueboybanana@gmail.com>
2020-08-12 19:30:32 +0300

    a lot of all

--- api/tea/objects.py
+++ api/tea/objects.py
@@ -1,24 +1,54 @@
 import random
 
-
 list = []
-for i in range(random.randint(0, 100)):
+for i in range(random.randint(0, 50)):
     list.append({
-    "teatype": random.randint(1, 11),
-    "capacity": random.randint(250, 1000),
-    "water": random.randint(100, 1000)
-})
-print(list)
-
+        "teatype": random.randint(1, 11),
+        "capacity": random.randint(0, 49),
+        "water": random.randint(0, 50)
+    })
+print(12, list)
 
 '''sort water from biggest to smallest'''
-def getValue(list):
-    return list['water']
 
 
-list.sort(key=getValue, reverse=True)
-print(list)
+def bubbleSortWater(list):
+    for index, element in enumerate(list):
+        for index2, compare_element in enumerate(list):
+            if list[index]['water'] < list[index2]['water']:
+                list[index], list[index2] = list[index2], list[index]
+    return list
 
-# '''teatype, capacity, water, list => return True or False in bool look using sort, decrease increase'''
-# def increaseDecrease(teatype, capacity, water, list):
-#     list.sort(())
+
+print(5, bubbleSortWater(list))
+
+'''bubble sort'''
+
+
+def BubbleSort(array):
+    for ind2, i in enumerate(array):
+        for ind, j in enumerate(array):
+            if len(array) - 1 == ind:
+                break
+            if array[ind2] < array[ind]:
+                array[ind2], array[ind] = array[ind], array[ind2]
+    return array
+
+
+array = []
+for i in range(random.randint(0, 20)):
+    array.append(random.randint(0, 20))
+print(array)
+
+print(BubbleSort(array))
+
+'''filter teatype: '''
+'''input: number => output: all keys with that value'''
+
+#
+# def FilterTeaType(list):
+a = filter(lambda element: element['teatype'] == 2, list)
+print(123, a)
+
+# o = int(input("What element do you want to return?: "))
+# print(1, FilterTeaType(list))
